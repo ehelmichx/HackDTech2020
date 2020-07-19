e9205435386004099fe4b428308131ab1bda5b23
ech56 <ech56@duke.edu>
2020-07-18 22:44:10 -0700

    added new view controllers and scenario images

---
+++ DTechHack2020/DTechHack2020/BarViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BarViewController.swift
+//  DTechHack2020
+//
+//  Created by Emma Helmich on 7/18/20.
+//  Copyright © 2020 Sophia Patterson. All rights reserved.
+//
+
+import UIKit
+
+class BarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ DTechHack2020/DTechHack2020/prefViewController.swift
@@ -0,0 +1,33 @@
+//
+//  prefViewController.swift
+//  DTechHack2020
+//
+//  Created by Emma Helmich on 7/18/20.
+//  Copyright © 2020 Sophia Patterson. All rights reserved.
+//
+
+import UIKit
+
+class prefViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(red: 226/255.0, green: 214/255.0, blue: 202/255.0, alpha: 1.0)
+        
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
